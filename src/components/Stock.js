c5dc346d2bc2f9c7b574c0f359ffae958102398f
ARauf Git <umiasad2017@gmail.com>
2020-05-06 01:13:04 -0400

    added code to call api

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -8,6 +8,26 @@ export class Stock extends Component {
             stockChartYValue: []
         }
     }
+
+    componentDidMount(){
+        this.fetchStock();
+    }
+
+    fetchStock(){
+        const API_KEY = 'UN4Q3M7CQJEGQH6W';
+        let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&apikey=${API_KEY}`;
+        fetch(API_CALL)
+        .then(
+            function(response){
+                return response.json();
+            }
+        )
+        .then(
+            function(data){
+                console.log(data)
+            }
+        )
+    }
     render() {
         return (
             <div>

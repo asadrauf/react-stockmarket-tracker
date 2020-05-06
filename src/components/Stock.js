a3239e2cc6cd955639a877fc8d165c40ee42d6a5
ARauf Git <umiasad2017@gmail.com>
2020-05-06 06:00:38 -0400

    hard coded stock code will display data. Search still not working

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,56 +1,100 @@
-import React, { Component } from 'react';
-import Plot from 'react-plotly.js';
+import React from 'react';
+import SearchBar from './SearchBar';
+import Plotly from "plotly.js"
+import createPlotlyComponent from 'react-plotly.js/factory';
+const Plot = createPlotlyComponent(Plotly);
 
-export class Stock extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            stockChartXValue: [],
-            stockChartYValue: []
-        }
+class Stock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        stockChartXValues: [],
+        stockChartYValues: [],
+        term: null,
+      value: ''
+      }
+      this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    }
+  
+    componentDidMount() {
+      this.fetchStock();
     }
 
-    componentDidMount(){
-        this.fetchStock();
+    handleChange(e) {
+        this.setState({
+          value: e.target.value,
+          term: this.state.value
+        });
+      }
+    
+      handleClick(e) {
+        if(e) e.preventDefault();
+        this.setState({
+          value: '',
+          term: this.state.value
+        });
+    
     }
-
-    fetchStock(){
-        const pointer = this;
-        const API_KEY = 'UN4Q3M7CQJEGQH6W';
-        let stockSymbol = 'AMZN';
-        let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=5min&outputsize=full&apikey=${API_KEY}`;
-        
-        let XvalFunc = [];
-        let YvalFunc = [];
-        fetch(API_CALL)
+    fetchStock() {
+      const pointerToThis = this;
+      
+      console.log(pointerToThis);
+      const API_KEY = 'HGJWFG4N8AQ66ICD';
+     
+      let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=AMZN&outputsize=compact&apikey=${API_KEY}`;
+      let stockChartXValuesFunction = [];
+      let stockChartYValuesFunction = [];
+  
+      fetch(API_Call)
         .then(
-            function(response){
-                return response.json();
-            }
+          function(response) {
+            return response.json();
+          }
         )
         .then(
-            function(data){
-                console.log(data)
-                for(var key in data['Time Series (Daily)']){
-                    XvalFunc.push(key);
-                    YvalFunc.push(data['Time Series (Daily'][key]['1. open']);
-                }
-                pointer.setState({
-                    stockChartXValue: XvalFunc,
-                    stockChartYValue: YvalFunc
-                });
+          function(data) {
+            console.log(data);
+  
+            for (var key in data['Time Series (Daily)']) {
+              stockChartXValuesFunction.push(key);
+              stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
             }
+  
+            // console.log(stockChartXValuesFunction);
+            pointerToThis.setState({
+              stockChartXValues: stockChartXValuesFunction,
+              stockChartYValues: stockChartYValuesFunction
+            });
+          }
         )
     }
+
+  
     render() {
-        return (
-            <div>
-                <h1>Stock Market</h1>
-           
-            </div>
-        )
+        const value = this.state.value;
+      return (
+        <div>
+          <h1>Stock Market</h1>
+          <SearchBar value={ value }
+                   onChange={ this.handleChange }
+                   onClick={ this.handleClick }/>
+          <Plot
+            data={[
+              {
+                x: this.state.stockChartXValues,
+                y: this.state.stockChartYValues,
+                type: 'scatter',
+                mode: 'lines+markers',
+                marker: {color: 'red'},
+              }
+            ]}
+            layout={{width: 720, height: 440, title: 'Stock Market Data'}}
+          />
+        </div>
+      )
     }
-}
-
-export default Stock
+  }
+  
+  export default Stock;
 
